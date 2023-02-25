60b808729c6badfc97ce43bd43cc0e01de6632a1
SabiMantock <sabimantock@gmail.com>
2023-02-25 12:53:54 +0000

    updated readme

---
+++ src/tests/App.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from '../App';
+import store from '../redux/store';
+
+describe('Root component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    div.id = 'root';
+    const root = ReactDOM.createRoot(div);
+
+    root.render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </Provider>
+      </React.StrictMode>,
+    );
+
+    root.unmount();
+  });
+});
